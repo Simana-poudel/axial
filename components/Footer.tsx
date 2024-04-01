bd83ac159e03ba9efd2a37ee4c6f1eee599b4f4c
Simana <poudelsimana22@gmail.com>
2024-04-01 11:23:24 +0545

    footer changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,33 +4,36 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import MediaLinks from "./MediaLinks";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
 import { FaLocationDot, FaSquareInstagram } from "react-icons/fa6";
 import { AiFillTwitterCircle } from "react-icons/ai";
-import { RiWhatsappFill } from "react-icons/ri";
-import { IoIosCall, IoIosSend } from "react-icons/io";
+import { RiFacebookFill, RiWhatsappFill } from "react-icons/ri";
+import { IoIosCall, IoIosSend, IoLogoSkype } from "react-icons/io";
 import { MdMail } from "react-icons/md";
+import { GrInstagram } from "react-icons/gr";
 
 const Footer = () => {
   return (
     <div className="flex items-center bg-sky-blue md:pt-10 bg-primary-1">
       <div className=" md:flex max-w-7xl mx-auto w-full  flex-col items-center ">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
-          <div>
+          <div className="flex-col">
             <div className="">
               <Image
                 src={"/images/ALD Logo/PNG/Artboard 1@4x.png"}
                 alt="Logo"
-                width={150}
+                width={190}
                 height={30}
               />
             </div>
-            <div className="">
-              <div className="flex gap-3 md:gap-4">
-                <FaFacebook className=" size-8 md:size-10 text-primary-2" />
-                <FaSquareInstagram className=" size-8 md:size-10 text-primary-2" />
-                <AiFillTwitterCircle className=" size-8 md:size-10 text-primary-2" />
-                <RiWhatsappFill className=" size-8 md:size-10 text-primary-2" />
+            <div className="self-end mt-8">
+              <div className="flex">
+                <RiFacebookFill className=" size-8 md:size-10 text-gray-500 bg-gray-700 p-[10px] m-1" />
+                <FaTwitter className=" size-8 md:size-10 text-gray-500 bg-gray-700 p-[10px] m-1" />
+
+                <IoLogoSkype className=" size-8 md:size-10 text-gray-500 bg-gray-700 p-[10px] m-1" />
+                <MdMail className=" size-8 md:size-10 text-gray-500 bg-gray-700 p-[10px] m-1" />
+                <GrInstagram className=" size-8 md:size-10 text-gray-500 bg-gray-700 p-[10px] m-1" />
               </div>
             </div>
           </div>
@@ -75,35 +78,35 @@ const Footer = () => {
                 to span your inbox.
               </p>
               <form>
-                <div className="flex flex-col space-y-1.5">
+                <div className="flex bg-white text-black items-center px-1">
                   <Input
-                    className="rounded-none"
-                    id="name"
+                    className="rounded-none w-96 mt-1 mr-2 border-none"
+                    id="email"
                     placeholder="Your Email"
-                  >
-                    {/* <IoIosSend /> */}
-                  </Input>
+                  ></Input>
+
+                  <IoIosSend className="bg-black size-10 text-white h-full cursor-pointer  " />
                 </div>
               </form>
             </div>
           </div>
         </div>
 
-        <div className="w-full md:w-full  h-[1px] bg-white my-10" />
+        <div className="w-full md:w-[30%]   h-[1px] bg-gray-200 my-10" />
         <div className="md:grid md:grid-cols-2 md:w-full">
-          <div className="font-medium text-white text-[17px] mb-3">
-            <p className="block m-6">
-              © BLACK TECH PVT.LTD.
-              <br />
-              All rights reserved Copyrights 2024.
+          <div className="font-light text-gray-200 mb-3 text-[17px]">
+            <p className="block">
+              Copyright &copy; 2024 ALD Corporate. All Right Reserved.
             </p>
           </div>
-          <div className="md:flex hidden justify-end">
+          <div className="md:flex hidden text-gray-200 justify-end">
             <div className="flex gap-3 md:gap-4">
-              <FaFacebook className=" size-8 md:size-10 text-primary-2" />
-              <FaSquareInstagram className=" size-8 md:size-10 text-primary-2" />
-              <AiFillTwitterCircle className=" size-8 md:size-10 text-primary-2" />
-              <RiWhatsappFill className=" size-8 md:size-10 text-primary-2" />
+              <div className="ml-3 font-light">
+                <p className="block"> Privacy policy.</p>
+              </div>
+              <div className="ml-3 font-light">
+                <p className="block"> Terms of use</p>
+              </div>
             </div>
           </div>
         </div>
