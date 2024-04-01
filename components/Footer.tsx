9859666c284e524ffa32914135bac94d3b7a650d
Simana <poudelsimana22@gmail.com>
2024-04-01 10:45:21 +0545

    favicon changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,17 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import MediaLinks from "./MediaLinks";
 import { FaFacebook } from "react-icons/fa";
-import { FaSquareInstagram } from "react-icons/fa6";
+import { FaLocationDot, FaSquareInstagram } from "react-icons/fa6";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { RiWhatsappFill } from "react-icons/ri";
+import { IoIosCall, IoIosSend } from "react-icons/io";
+import { MdMail } from "react-icons/md";
 
 const Footer = () => {
   return (
     <div className="flex items-center bg-sky-blue md:pt-10 bg-primary-1">
       <div className=" md:flex max-w-7xl mx-auto w-full  flex-col items-center ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
           <div>
             <div className="">
               <Image
@@ -23,49 +25,54 @@ const Footer = () => {
                 height={30}
               />
             </div>
-            <p className=" text-lg text-white">
-              Molestie nunc non blandit massa. Lacus sed viverra tellus in hac
-              habitasse platea. Velit egestas
-            </p>
-          </div>
-          <div className=" md:mx-6">
             <div className="">
-              <h1 className="text-white text-2xl mb-7 font-bold">Contact Us</h1>
-              <div className="font-medium text-white text-[17px]">
-                <p className="block">+23 333 4444 555</p>
-                <p className="block mb-3">+23 333 4444 555</p>
-                <p className="block">2047, Cyrus Viaduct</p>
-                <p className="block mb-3">East Janayster</p>
+              <div className="flex gap-3 md:gap-4">
+                <FaFacebook className=" size-8 md:size-10 text-primary-2" />
+                <FaSquareInstagram className=" size-8 md:size-10 text-primary-2" />
+                <AiFillTwitterCircle className=" size-8 md:size-10 text-primary-2" />
+                <RiWhatsappFill className=" size-8 md:size-10 text-primary-2" />
               </div>
             </div>
           </div>
-          <div className="md:mx-6">
-            <h1 className="text-white text-2xl mb-7 font-bold">Company</h1>
-            <div className="font-medium text-white text-[17px]">
-              <a className="block mb-3" href="#">
-                About
-              </a>
-              <a className="block mb-3" href="#">
-                Service
-              </a>
-              <a className="block mb-3" href="#">
-                Project
-              </a>
-              <a className="block mb-3" href="#">
-                Blog
-              </a>
-              <a className="block mb-3" href="#">
-                Request/Quote
-              </a>
+          <div className=" md:mx-6">
+            <div className="">
+              <h1 className="text-white text-2xl mb-7 font-bold">
+                Address Company
+              </h1>
+              <div className="font-medium text-gray-200 text-[17px]">
+                <div className="flex">
+                  <FaLocationDot className="text-blue-900 mt-2" />
+                  <div className="ml-3 font-light">
+                    <p className="block">37 San Juan Lane</p>
+                    <p className="block mb-3">
+                      Graff Florisstraat 22A, 3021 CH
+                    </p>
+                  </div>
+                </div>
+                <div className="flex mb-3">
+                  <IoIosCall className="text-blue-900 size-5" />
+                  <div className="ml-3 font-light">
+                    <p className="block"> Call Us: +1.809.1206705</p>
+                  </div>
+                </div>
+                <div className="flex mb-3">
+                  <MdMail className="text-blue-900 size-5" />
+                  <div className="ml-3 font-light">
+                    <p className="block"> Mail: Graconstruction@gmail.com</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+
           <div className="">
             <h1 className="text-white text-2xl mb-4 font-bold">
               Subscribe to Our News
             </h1>
-            <div className="font-medium text-white text-[17px] mb-3">
+            <div className="font-light text-gray-200 text-[17px] mb-3">
               <p className="block mb-6">
-                Subscribe our newsletter to get updates about our services
+                Subscribe to receive the latest news from us, we make sure not
+                to span your inbox.
               </p>
               <form>
                 <div className="flex flex-col space-y-1.5">
@@ -73,10 +80,9 @@ const Footer = () => {
                     className="rounded-none"
                     id="name"
                     placeholder="Your Email"
-                  />
-                  <Button className="bg-primary-2 mb-4 uppercase text-black font-bold text-lg hover:bg-primary-2 px-8 w-full rounded-none">
-                    Suscribe
-                  </Button>
+                  >
+                    {/* <IoIosSend /> */}
+                  </Input>
                 </div>
               </form>
             </div>
